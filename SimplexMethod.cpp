1277ac008a85f8b93f387719fc20f829be003adb
daniil <4shushakov@gmail.com>
2021-05-22 23:33:04 +0700

    Encapsulate into SimplexMethod class, add SimplexModel and SimplexAnswer structs

--- SimplexMethod.cpp
+++ SimplexMethod.cpp
@@ -1,11 +1,7 @@
 #include <cfloat>
-#include <cstring>
 #include <fstream>
 #include <iostream>
 #include <vector>
-
-#define FILE_INPUT 1
-#define FILE_NAME "D:\\source\\plus_plus\\igri\\in.txt"
 
 using namespace std;
 
@@ -13,83 +9,103 @@ typedef double db;
 typedef vector<db> dvector;
 typedef vector<dvector> dmatrix;
 
-// Получить разрешающий столбец
-int get_permissive_col(const dvector& L)
-{
-    int col = -1;
-    db mn = 0;
-    for (int i = 1; i < L.size(); i++)
-        if (L[i] < mn) {
-            mn = L[i];
-            col = i;
-        }
-    return col;
-}
+struct SimplexModel {
+    dvector c, b;
+    dmatrix a;
+    bool isMax = false;
+};
 
-// Получить разрешающую строку
-int get_permissive_row(const dmatrix& table, int k)
-{
-    int row = -1;
-    db min_value = DBL_MAX;
-    for (int i = 1; i < table.size(); i++)
-        if (table[i][k] > 0 && table[i][0] / table[i][k] < min_value) {
-            row = i;
-            min_value = table[i][0] / table[i][k];
-        }
-    return row;
-}
+struct SimplexAnswer {
+    dvector x;
+    db f;
+};
 
-// Решить симплекс-методом
-dmatrix solve_simplex(dmatrix table, db* basis)
-{
-    auto n = table[0].size();
-    auto m = table.size();
-    auto k = 0;
-    vector<int> main_basis(m - 1), not_main_basis(n - 1);
+class SimpexMethod {
+public:
+    SimpexMethod(SimplexModel sm) : sm(std::move(sm)) {}
 
-    for (int i = 0; i < n - 1; i++)
-        not_main_basis[i] = i;
-    for (int i = n - 1; i < n + m - 2; i++)
-        main_basis[i - n + 1] = i;
-    while ((k = get_permissive_col(table[0])) != -1) {
-        // k-ый столбец разрешающий
-        // r-ая строка разрешающая
-        int r = get_permissive_row(table, k);
-        if (r == -1)
-            return table;
-        swap(main_basis[r - 1], not_main_basis[k - 1]);
+    SimplexAnswer Solve() {
+        ConvertModelToTable();
 
-        // Преобразуем разрешающий элемент
-        table[r][k] = 1. / table[r][k];
-        // Преобразуем разрешающий столбец
-        for (int i = 0; i < m; i++)
-            if (i != r)
-                table[i][k] *= -table[r][k];
-        // Преобразуем остальные элементы
-        for (int i = 0; i < m; i++)
+        vector<int> main_basis(m - 1), not_main_basis(n - 1);
+        for (auto i = 0; i < n - 1; ++i)
+            not_main_basis[i] = i;
+        for (auto i = n - 1; i < n + m - 2; ++i)
+            main_basis[i - n + 1] = i;
+
+        auto k = GetPermissiveCol();
+        auto r = -1;
+        if (k != -1) r = GetPermissiveRow(k);
+
+        while (k != -1 && r != -1) {
+            // k-th column, r-th row are permissive
+            swap(main_basis[r - 1], not_main_basis[k - 1]);
+            // Modify permissive element
+            table[r][k] = 1. / table[r][k];
+            // Modify permissive column
+            for (int i = 0; i < m; i++)
+                if (i != r)
+                    table[i][k] *= -table[r][k];
+            // Modify other elements
+            for (int i = 0; i < m; i++)
+                for (int j = 0; j < n; j++)
+                    if (i != r && j != k)
+                        table[i][j] += table[r][j] * table[i][k];
+            // Modify permissive row
             for (int j = 0; j < n; j++)
-                if (i != r && j != k)
-                    table[i][j] += table[r][j] * table[i][k];
-        // Преобразуем разрешающую строку
-        for (int j = 0; j < n; j++)
-            if (j != k)
-                table[r][j] *= table[r][k];
-    }
-    for (int i = 0; i < m - 1; i++)
-        basis[main_basis[i]] = table[i + 1][0];
-    return table;
-}
+                if (j != k)
+                    table[r][j] *= table[r][k];
 
-dmatrix create_table(const dvector& c, const dvector& b, const dmatrix& a, int is_max)
-{
-    int m = a.size() + 1, n = c.size();
-    dmatrix table(m, dvector(n));
-    for (int j = 0; j < n; j++)
-        table[0][j] = -c[j] * is_max;
-    for (int i = 1; i < m; i++)
-        table[i][0] = b[i - 1];
-    for (int i = 1; i < m; i++)
-        for (int j = 1; j < n; j++)
-            table[i][j] = a[i - 1][j - 1];
-    return table;
-}
+            k = GetPermissiveCol();
+            if (k != -1) r = GetPermissiveRow(k);
+        }
+
+        SimplexAnswer sa{dvector(n + m - 2)};
+        for (int i = 0; i < m - 1; i++)
+            sa.x[main_basis[i]] = table[i + 1][0];
+        sa.f = table[0][0];
+        return sa;
+    }
+
+
+private:
+    /// Получить разрешающий столбец
+    int GetPermissiveCol() {
+        int col = -1;
+        db mn = 0;
+        for (int i = 1; i < table[0].size(); i++)
+            if (table[0][i] < mn) {
+                mn = table[0][i];
+                col = i;
+            }
+        return col;
+    }
+
+    int GetPermissiveRow(int k) {
+        int row = -1;
+        db min_value = DBL_MAX;
+        for (int i = 1; i < table.size(); i++)
+            if (table[i][k] > 0 && table[i][0] / table[i][k] < min_value) {
+                row = i;
+                min_value = table[i][0] / table[i][k];
+            }
+        return row;
+    }
+
+    dmatrix ConvertModelToTable() {
+        m = (int)sm.b.size() + 1, n = (int)sm.c.size();
+        table.resize(m, dvector(n));
+        for (int j = 0; j < n; j++)
+            table[0][j] = -sm.c[j] * sm.isMax;
+        for (int i = 1; i < m; i++)
+            table[i][0] = sm.b[i - 1];
+        for (int i = 1; i < m; i++)
+            for (int j = 1; j < n; j++)
+                table[i][j] = sm.a[i - 1][j - 1];
+        return table;
+    }
+
+    SimplexModel sm;
+    dmatrix table;
+    int n = 0, m = 0;
+};
