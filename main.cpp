d4fe7ae056cb7e46160374b546d59cf41dcaf59f
daniil <4shushakov@gmail.com>
2021-05-23 18:06:05 +0700

    Handle bad input data

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <exception>
 #include "SimplexMethod.h"
 
 std::string GetInputFileName() {
@@ -7,11 +8,11 @@ std::string GetInputFileName() {
     return fileName;
 }
 
-SimplexModel ConvertToSimplexModel(const dmatrix& matrix){
+SimplexModel ConvertToSimplexModel(const dmatrix &matrix) {
     SimplexModel model;
     model.isMax = true;
     model.a = matrix;
-    model.c.resize(matrix[0].size()+1, 1);
+    model.c.resize(matrix[0].size() + 1, 1);
     model.c[0] = 0;
     model.b.resize(matrix.size(), 1);
     return model;
@@ -19,12 +20,14 @@ SimplexModel ConvertToSimplexModel(const dmatrix& matrix){
 
 dmatrix Input() {
     std::ifstream fin(GetInputFileName());
-    int m, n;
+    int m = 0, n = 0;
     fin >> n >> m;
     dmatrix matrix(m, dvector(n));
     for (int i = 0; i < m; ++i)
         for (int j = 0; j < n; ++j)
             fin >> matrix[i][j];
+    if (fin.fail())
+        throw std::runtime_error("Bad Input data");
     return matrix;
 }
 
